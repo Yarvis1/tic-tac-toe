614b1ec5e4284bffa7421b17349abff725ea6a5d
zach <418926@bsd48.org>
2024-10-10 14:44:37 -0700

    added play again and win counter

--- file.cpp
+++ file.cpp
@@ -10,7 +10,10 @@ void printBoard(char board[3][3]);
 void place(char board[3][3], char &turn);
 // initalize board and turn + while loop for turns
 int main() {
-    char board[3][3] = {
+  char playAgain='\n';
+  int xWins=0;
+  int oWins=0;
+  char board[3][3] = {
         {'-','-','-'},
         {'-','-','-'},
         {'-','-','-'}
@@ -22,10 +25,30 @@ int main() {
     }
     if (checkwin(board) == 1) {
         cout << "Player x wins!" << endl;
-    } else if (checkwin(board) == 2) {
-        cout << "Player o wins!" << endl;
+	xWins++;
+	cout<< "o won: "<<oWins<<" times."<<endl;
+	cout<< "x won: "<<xWins<<" times. play again?(y/n)"<<endl;
+        cin>>playAgain;
+        if (playAgain=='y'){
+          main();
+        }
+        else{
+          exit(0);
+        }
     }
-    return 0;
+     else if (checkwin(board) == 2) {
+        cout << "Player o wins!" << endl;
+	oWins++;
+	cout<< "x won: "<<xWins<<" times."<<endl;
+	cout<< "o won: "<<oWins<<" times. play again?(y/n)"<<endl;
+	cin>>playAgain;
+	if (playAgain=='y'){
+	  main();
+	}
+	else{
+	  exit(0);
+	}
+}
 }
 //place
 void place(char board[3][3], char &turn) {  // pass by refrence
@@ -70,7 +93,7 @@ int checkwin(char board[3][3]){
   if(board[0][0]=='x' && board[1][1]=='x'&& board[2][2]=='x'){
     return 1;
   }
-  if(board[2][2]=='x' && board[1][1]=='x'&& board[0][2]=='x'){
+  if(board[2][0]=='x' && board[1][1]=='x'&& board[0][2]=='x'){
     return 1;
   }
   if(board[0][0]=='o' && board[0][1]=='o'&& board[0][2]=='o'){
@@ -94,7 +117,7 @@ int checkwin(char board[3][3]){
   if(board[0][0]=='o' && board[1][1]=='o' && board[2][2]=='o'){
     return 2;
   }
-  if(board[2][2]=='o' && board[1][1]=='o'&& board[0][2]=='o'){
+  if(board[2][0]=='o' && board[1][1]=='o'&& board[0][2]=='o'){
     return 2;
   }
   return 0;
